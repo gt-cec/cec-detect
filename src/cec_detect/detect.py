d2ac2a1e60a2a0983d850d1bec42db08d33a0442
Jack Kolb <jack@jackkolb.com>
2025-04-19 14:49:06 -0400

    integrated segmentation into detect

--- src/cec_detect/detect.py
+++ src/cec_detect/detect.py
@@ -24,6 +24,30 @@ class Detector():
         self.processor = Owlv2Processor.from_pretrained("google/owlv2-base-patch16-ensemble")
         self.model = Owlv2ForObjectDetection.from_pretrained("google/owlv2-base-patch16-ensemble").to(torch.device(self.device))
 
+        # load the segmentation model
+        try:
+            from sam2.build_sam import build_sam2
+            from sam2.sam2_image_predictor import SAM2ImagePredictor
+
+            # adding SAM2 config path from here: https://github.com/facebookresearch/sam2/issues/81#issuecomment-2262979343
+            # import hydra
+            # hydra is initialized on import of sam2, which sets the search path which can't be modified
+            # so we need to clear the hydra instance
+            # hydra.core.global_hydra.GlobalHydra.instance().clear()
+            # reinit hydra with a new search path for configs
+            # hydra.initialize_config_module('', version_base='1.2')/Users/jack/Documents/Installations/sam2
+
+            # this should work now
+            # model = build_sam2('<config-name>', '<checkpoint-path>')
+
+            sam2_checkpoint = "./segmentation/sam2.1_hiera_large.pt"
+            model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
+            sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=self.device)
+            self.predictor = SAM2ImagePredictor(sam2_model)
+        except ModuleNotFoundError as e:
+            print("[CEC Detect] WARNING: SAM2 is not installed, do not expect to use it. Message:", e)
+            self.predictor = None
+
     # main detection function
     def detect(self, image, classes, threshold=0.1):
         texts = [["" + c for c in classes]]
@@ -81,7 +105,36 @@ class Detector():
         objects = [x for x in objects if x is not None]  # remove the objects we filtered out
 
         return objects
+    
+    # given a bounding box, get the segmentation
+    # inputs: RGB image, detected bounding boxes
+    # outputs: segmentation masks (should be 1 per bounding box)
+    def segment(self, rgb, boxes):
+        """
+        Given an RGB image and a list of bounding boxes, run a model to segment the bounding boxes. Uses SAM2.
 
+        Inputs:
+            rgb: Image in typical OpenCV RGB format: numpy array (H,W,3)
+            boxes: List of N bounding boxes: [[[col1, row1], [col2, row2]], ... ]
+
+        Output:
+            masks: segmentation masks for each of the N boxes stacked: numpy array (N,H,W)
+        """
+        # edge case: no boxes, return with no masks
+        if len(boxes) == 0:
+            return np.array([])
+        # check if predictor is None (mean's SAM2 is not installed)
+        assert self.predictor is not None, "The segmentation system is being called, however SAM2 is not installed. Install SAM2 first."
+
+        self.predictor.set_image(rgb)  # give the image to the predictor
+        masks, scores, _ = self.predictor.predict(
+            point_coords=None,
+            point_labels=None,
+            box=boxes,
+            multimask_output=False,
+        )  # predict the segmentations using the bounding boxes
+        masks = masks.reshape((masks.shape[0], rgb.shape[0], rgb.shape[1]))  # N x 1 x 512 x 512 to N x 512 x 512
+        return masks == 1  # convert from 0's and 1's to False and True
 
     # removes boxes from detected object set A (main set) that are not in detected object set B (check set)
     # this is used for syncing RGB and depth detected objects
